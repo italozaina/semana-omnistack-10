920cf97e30968a1899e504fb2bb46d62a23be0eb
Ítalo Zaina <italo.zaina@contabexpress.com.br>
2020-01-14 12:53:30 -0300

    Recebiemnto de parametros via body, configuração do express para identificar JSON

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json()); // Entender requisições com JSON no body
+
 // Métodos HTTP: GET, POST, PUT, DELETE
 
 // TIpos de parâmetros:
 
 // Query Params: request.query (Filtros, ordenação, paginação ...) ?search=Diego
 // Route Params: request.params ex.: (Identificar um recurso na alteração ou remoção) http://localhost:3333/users/1    <- parametro direto na rota
-// Body
+// Body: request.body (Dados para criação ou alteração de um registro)
 
-app.delete('/users/:id', (request, response) => {
-    console.log(request.params)
+app.post('/users', (request, response) => {
+    console.log(request.body)
     return response.json({message: 'Hello World'});
 });
 
